d45681fd3510b0ca86db5f8266a22b3e82a97e78
nadia70 <41780932+Iradukunda-Nadia@users.noreply.github.com>
2020-01-16 03:23:05 +0300

    Initial commit

---
+++ lib/admin.dart
@@ -0,0 +1,113 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class Admin extends StatefulWidget {
+  @override
+  _AdminState createState() => _AdminState();
+}
+
+class _AdminState extends State<Admin> {
+
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  File videoFile;
+
+  @override
+  void initState() {
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin'),
+      ),
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
+      body: Column(
+        children: <Widget>[
+          Visibility(
+            visible: _controller != null,
+            child: FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          RaisedButton(
+            child: Text("Video"),
+            onPressed: () {
+              getVideo();
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: _controller == null
+          ? null
+          : FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future getVideo() async {
+    Future<File> _videoFile =
+    ImagePicker.pickVideo(source: ImageSource.gallery);
+    _videoFile.then((file) async {
+      setState(() {
+        videoFile = file;
+        _controller = VideoPlayerController.file(videoFile);
+
+        // Initialize the controller and store the Future for later use.
+        _initializeVideoPlayerFuture = _controller.initialize();
+
+        // Use the controller to loop the video.
+        _controller.setLooping(true);
+      });
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:fgck_app/admin.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'F.G.C.K APP',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.indigo,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Admin(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+
+                Colors.white,
+                Colors.white,
+                Colors.indigo[50],
+                Colors.blue[50],
+                Colors.indigoAccent,
+                Colors.deepPurple[900],
+              ],
+            ),
+          ),
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              SizedBox(
+                height: 30.0,
+              ),
+
+              Text("FULL GOSPEL CHURCH OF KENYA",textAlign: TextAlign.center, style: TextStyle(fontSize: 23.0, letterSpacing: -1.0, wordSpacing: 2.0, ),),
+
+              Image.asset(
+                'assets/fgck.png',
+                fit: BoxFit.contain,
+                height: 200.0,
+                width: 200.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+
+
+
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(10, 10, 20, 0),
+                                child: MaterialButton(
+                                  onPressed: (){},
+                                  child: Text('Home',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'SFUIDisplay',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  color: Colors.white,
+                                  elevation: 16.0,
+                                  minWidth: 400,
+                                  height: 50,
+                                  textColor: Colors.deepPurple[900],
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(9.0)
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(10, 10, 20, 0),
+                                child: MaterialButton(
+                                  onPressed: (){},
+                                  child: Text('Admin',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'SFUIDisplay',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  color: Colors.white,
+                                  elevation: 16.0,
+                                  minWidth: 400,
+                                  height: 50,
+                                  textColor: Colors.deepPurple[900],
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(9.0)
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
