4a94551b32d42b4c9768ba1c58a29bf9b58eaf1a
nadia70 <41780932+Iradukunda-Nadia@users.noreply.github.com>
2020-01-28 07:25:14 +0300

    admin changes

---
+++ functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+
+admin.initializeApp();
+
+const fcm = admin.messaging();
+
+export const sendToTopic = functions.firestore
+       .document('videos/{Item}')
+       .onCreate(async snapshot => {
+
+         const message: admin.messaging.MessagingPayload = {
+           notification: {
+             title: 'New Sermon!',
+             body: `A new sermon has been uploaded`,
+           }
+         };
+
+         return fcm.sendToTopic('sermon', message);
+       });
